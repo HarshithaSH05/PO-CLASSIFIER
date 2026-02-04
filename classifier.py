7cce5f55f4187e66e04015bf61590dcbf831cc09
HarshithaSH05 <shharshitha05@gmail.com>
2026-02-04 09:42:27 +0530

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from groq import Groq
+from prompts import SYSTEM_PROMPT
+
+client=Groq(
+    api_key=st.secrets["GROQ_API_KEY"]
+)
+
+MODEL="openai/gpt-oss-120b"
+
+def classify_po(po_description: str, supplier: str="Not provided"):
+    user_prompt=f"""
+PD Description:
+{po_description}
+
+Supplier:
+{supplier}
+"""
+    response = client.chat.completion.create(
+        model=MODEL,
+        temperature=0.0,
+         messages=[
+             {"role": "system", "content": SYSTEM_PROMPT},
+             {"role": "user", "content": user_prompt}
+        ]
+    )
+    return Response.choices[0].message.content
+    
